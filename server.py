acb731b7f4ad1aa6c3267bb03f4eb4671bef7db1
Craig Schindler <craig.schindler@berkeley.edu>
2020-03-30 21:04:14 -0700

    .

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
-
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world:
-	return "hello world"
+def hello_world():
+    return 'Hello, World!'
 
 '''
 def getSquareRoot():
